6b07a2c510594c72888b542e61314350a68762fc
Yakami22 <yassine2000.yk@gmail.com>
2023-01-05 19:04:22 +0100

    Updated Methods

--- src/aeroport/Airplane.java
+++ src/aeroport/Airplane.java
@@ -19,18 +19,33 @@ public class Airplane extends GenericSimEntity {
 	protected void init() {
 		super.init();
 		postBehaviour(getEngine().SimulationDate(), this::landing);
+		postBehaviour(getEngine().SimulationDate(), this::takeOff);
 	}
 	
 	public void landing() {
-		Logger.Information(this, getName(), "init");
+		Logger.Information(this, getName(), "landing");
 		List<EntiteSimulee> myAeroports = recherche(e -> ((e instanceof Aeroport)));
 		if(myAeroports.size()>0)
-			Logger.Information(this, "init", getName() + " a trouvé l'aeroport " + myAeroports.get(0).getName());
+			Logger.Information(this, "landing", getName() + " a trouvé l'aeroport " + myAeroports.get(0).getName());
 		//Aeroport a = (Aeroport) myAeroports.get(0) ;
 	}
 	
+//	public void takeOff() {
+//		Logger.Information(this, getName(), "takeOff");
+//		List<EntiteSimulee> myAirplanes = recherche(e-> ((e instanceof Airplane)));
+//		if(myAirplanes.size()>0)
+//			for( int i = 0 ; i<myAirplanes.size() ; i++)
+//			Logger.Information(this, "takeoff", getName() + " a décollé " + myAirplanes.get(i).getName());
+//		
+//	}
+	
 	public void takeOff() {
-		
+		Logger.Information(this, getName(), "takeOff");
+		List<EntiteSimulee> myAeroports = recherche(e -> ((e instanceof Aeroport)));
+		if(myAeroports.size()>0)
+			Logger.Information(this, "takeOff", getName() + " a décollé de l'aeroport " + myAeroports.get(0).getName());
+		//Aeroport a = (Aeroport) myAeroports.get(0) ;
 	}
+	
 
 }
